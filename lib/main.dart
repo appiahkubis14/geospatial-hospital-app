1d0cc442ffe8e9c5c4059a4871b54a40703b7d35
appiahkubis14 <appiahkubis14@gmail.com>
2024-11-04 09:20:22 +0000

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,5 @@
-<<<<<<< HEAD
-// ignore_for_file: depend_on_referenced_packages
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:hospitalmap/firebase_options.dart';
-import 'package:hospitalmap/social_auth/provider/internet_provider.dart';
-import 'package:hospitalmap/social_auth/provider/sign_in_provider.dart';
-import 'package:hospitalmap/social_auth/screens/splash_screen.dart';
-import 'package:hospitalmap/welcome/screens/welcome_screen.dart';
-import 'package:hospitalmap/welcome/theme/theme.dart';
-import 'package:provider/provider.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  if (!kDebugMode) {
-    await FirebaseAppCheck.instance.activate(
-      androidProvider: AndroidProvider.playIntegrity,
-      appleProvider: AppleProvider.appAttest,
-      webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
-    );
-  } else {
-    await FirebaseAppCheck.instance.activate(
-      androidProvider: AndroidProvider.debug,
-      appleProvider: AppleProvider.debug,
-    );
-  }
 
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: ((context) => SignInProvider()),
-        ),
-        ChangeNotifierProvider(
-          create: ((context) => InternetProvider()),
-        ),
-      ],
-      child: MaterialApp(
-        themeMode: ThemeMode.dark,
-        debugShowCheckedModeBanner: false,
-        theme: darkMode,
-        home: const SplashScreen(),
-      ),
-    );
-  }
-}
-=======
 import 'dart:io';
 import 'package:flutter/material.dart';
 // import 'package:hospital_locator/screen/newScreen.dart';
@@ -78,4 +25,3 @@ class MyApp extends StatelessWidget {
 }
 
 
->>>>>>> 7693faa (updated repo)
